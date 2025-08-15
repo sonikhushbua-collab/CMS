1fedc7840b0c2bb582f70b989d33cfa64931f36b
Khushbu A Soni <sonikhushbua@gmail.com>
2025-08-15 19:47:05 +0100

    Add files via upload

---
+++ application/logs/log-2021-10-01.php
@@ -0,0 +1,4 @@
+<?php defined('BASEPATH') OR exit('No direct script access allowed'); ?>
+
+ERROR - 2021-10-01 12:09:56 --> Severity: Notice --> Undefined variable: error C:\xampp\htdocs\phpscriptpoint\eventplus\eventplus\cms\application\controllers\Home.php 80
+ERROR - 2021-10-01 12:11:05 --> Severity: Notice --> Undefined variable: error C:\xampp\htdocs\phpscriptpoint\eventplus\eventplus\cms\application\controllers\Tickets.php 80
---
+++ application/logs/log-2025-03-20.php
@@ -0,0 +1,6 @@
+<?php defined('BASEPATH') OR exit('No direct script access allowed'); ?>
+
+ERROR - 2025-03-20 19:04:07 --> Severity: Warning --> mysqli::real_connect(): (HY000/1049): Unknown database 'eventplus' C:\xampp\htdocs\cms\system\database\drivers\mysqli\mysqli_driver.php 201
+ERROR - 2025-03-20 19:04:07 --> Unable to connect to the database
+ERROR - 2025-03-20 19:04:29 --> Query error: Table 'eventplus.tbl_language' doesn't exist - Invalid query: SELECT * FROM tbl_language
+ERROR - 2025-03-20 19:04:59 --> Query error: Table 'eventplus.tbl_slider' doesn't exist - Invalid query: SELECT * FROM tbl_slider WHERE id=1
